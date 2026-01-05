09bc33818b84882ca90200d60dc9bd4735e63f7e
Himanshu Singh <himanshusingh1k@gmail.com>
2026-01-05 13:38:39 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
